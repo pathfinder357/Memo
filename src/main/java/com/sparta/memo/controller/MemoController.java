eef26beaeaec8f2e8c2b463a4310ccd8b8b31831
Yujun <pathfinder357@github.com>
2026-06-23 09:54:13 +0900

    update Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -1,14 +1,15 @@
 package com.sparta.memo.controller;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class MemoController {
 	// 데이터가 바디 형태의 JSON으로 넘어옴 -> 그럼 어떻게 받지? requestBody로
 	// requestDto로 Body의 데이터를 받음 -> createMemo는 requestDto를 받아서 어떤 로직을 수행하는 함수
 	// MemoResponseDto는 반환타입
-	public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+	public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
 		// requestDto -> Entity(DB와 소통하는 클래스)
 		// memo 엔티티에 뭘 넣어야하지? requestBody로 부터 받아온 데이터를 담는
 		// requestDto(손님의 주문서)를 하나의 객체에 넣어야하지.
@@ -39,7 +40,7 @@ public class MemoController {
 		Memo memo = new Memo(requestDto);
 
 		// Memo Max ID Check
-		Long maxId = memoList.size() > 0  ? Collections.max(memoList.keySet()) + 1 : 1;
+		Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
 		memo.setId(maxId);
 
 		// DB 저장
@@ -55,12 +56,44 @@ public class MemoController {
 		return memoResponseDto;
 	}
 
+	// 조회 API
 	@GetMapping("/memos")
 	// List인 이유? Memo(MemoResponseDto)는 당연히 여러개일 가능성이 높으니깐
-	public List<MemoResponseDto> getMemos(){
+	public List<MemoResponseDto> getMemos() {
 		// Map to List
 		List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
 
 		return responseList;
 	}
+
+	// 업데이트 API
+	@PutMapping("/memos/{id}")
+	// 반환은 업데이트한 ID만 넘겨줌
+	// 데이터를 수정한다는 것은 데이터의 내용(ex:유저 네임)
+	// 따라서 그 데이터는 클라이언트에서 바디 부분에 JSON 형식으로 받아옴
+	// 따라서 @RequestBody가 당연히 필요
+	public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+		//해당 메모가 DB에 존재하는지 확인
+		if (memoList.containsKey(id)) {
+			// 해당 메모 가져오기
+			Memo memo = memoList.get(id);
+			memo.update(requestDto);
+			return memo.getId();
+		} else {
+			throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+		}
+	}
+
+	@DeleteMapping("/memos/{id}")
+	public Long deleteMemo(@PathVariable Long id) {
+		//해당 메모가 DB에 존재하는지 확인
+		if (memoList.containsKey(id)) {
+			// 해당 메모를 삭제
+			memoList.remove(id);
+			return id;
+		} else {
+			throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+		}
+	}
+
 }
--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -20,4 +20,9 @@ public class Memo {
 		this.username = requestDto.getUsername();
 		this.contents = requestDto.getContents();
 	}
+
+	public void update(MemoRequestDto requestDto) {
+		this.username = requestDto.getUsername();
+		this.contents = requestDto.getContents();
+	}
 }
